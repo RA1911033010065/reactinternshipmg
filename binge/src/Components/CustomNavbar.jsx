ba33078e2ed22df9a892cc8105ea5bcc415d95ed
Mohit Gupta <mg6489@srmist.edu.in>
2021-10-18 19:10:33 +0530

    final changes

--- binge/src/Components/CustomNavbar.jsx
+++ binge/src/Components/CustomNavbar.jsx
@@ -52,7 +52,7 @@ const CustomNavbar = () => {
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">
-                                    Home
+                                    HOME
                                 </a>
                             </li>
                             <li className="nav-item">
